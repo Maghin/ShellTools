443153f96fbc8be19c0422d595e6b89831ef5630
Maghin <realmaghin@hotmail.com>
2016-08-27 18:24:34 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -15,7 +15,7 @@
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
-# -> Prevents accidentally clobbering files.
+# -> Prevents accidentally clobbering files
 alias mkdir='mkdir -p'
 
 alias h='history'
@@ -28,7 +28,7 @@ alias path='echo -e ${PATH//:/\\n}'
 alias libpath='echo -e ${LD_LIBRARY_PATH//:/\\n}'
 
 
-alias du='du -kh'    # Makes a more readable output.
+alias du='du -kh'    # Makes a more readable output
 alias df='df -kTh'
 
 #-------------------------------------------------------------
@@ -36,17 +36,18 @@ alias df='df -kTh'
 #-------------------------------------------------------------
 # Add colors for filetype and  human-readable sizes by default on 'ls':
 alias ls='ls -h --color'
-alias lx='ls -lXB'         #  Sort by extension.
-alias lk='ls -lSr'         #  Sort by size, biggest last.
-alias lt='ls -ltr'         #  Sort by date, most recent last.
-alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
-alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
+alias lx='ls -lXB'        # Sort by extension
+alias lk='ls -lSr'        # Sort by size, biggest last
+alias lt='ls -ltr'        # Sort by date, most recent last
+alias lc='ls -ltcr'       # Sort by/show change time,most recent last
+alias lu='ls -ltur'       # Sort by/show access time,most recent last
+alias lz='ls -lZ'# Show selinux context
 
 # The ubiquitous 'll': directories first, with alphanumeric sorting:
 alias ll="ls -lv --group-directories-first"
 alias lm='ll |more'        #  Pipe through 'more'
-alias lr='ll -R'           #  Recursive ls.
-alias la='ll -A'           #  Show hidden files.
+alias lr='ll -R'           #  Recursive ls
+alias la='ll -A'           #  Show hidden files
 alias tree='tree -Csuh'    #  Nice alternative to 'recursive ls' ...
 
 
@@ -58,11 +59,11 @@ alias more='less'
 export PAGER=less
 export LESSCHARSET='latin1'
 export LESSOPEN='|/usr/bin/lesspipe.sh %s 2>&-'
-                # Use this if lesspipe.sh exists.
+                # Use this if lesspipe.sh exists
 export LESS='-i -N -w  -z-4 -g -e -M -X -F -R -P%t?f%f \
 :stdin .?pb%pb\%:?lbLine %lb:?bbByte %bb:-...'
 
-# LESS man page colors (makes Man pages more readable).
+# LESS man page colors (makes Man pages more readable)
 export LESS_TERMCAP_mb=$'\E[01;31m'
 export LESS_TERMCAP_md=$'\E[01;31m'
 export LESS_TERMCAP_me=$'\E[0m'
