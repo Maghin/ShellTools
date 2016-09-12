4eb2dd21c1e9631814a26e52b1e6574d2676e341
Maghin <realmaghin@hotmail.com>
2016-09-12 14:58:44 +0200

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,11 @@
+## PROXY PERSO
+MY_PROXY="http://monproxy.fr:3128"
+######################
+HTTP_PROXY=$MY_PROXY
+HTTPS_PROXY=$MY_PROXY
+FTP_PROXY=$MY_PROXY
+http_proxy=$MY_PROXY
+https_proxy=$MY_PROXY
+ftp_proxy=$MY_PROXY
+######################
+export HTTP_PROXY HTTPS_PROXY FTP_PROXY http_proxy https_proxy ftp_proxy
