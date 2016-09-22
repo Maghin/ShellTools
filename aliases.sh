3476aa611a1605911c9874c8fbbb9ca122418844
Maghin <realmaghin@hotmail.com>
2016-09-22 16:38:40 +0200

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -41,7 +41,7 @@ alias lk='ls -lSr'        # Sort by size, biggest last
 alias lt='ls -ltr'        # Sort by date, most recent last
 alias lc='ls -ltcr'       # Sort by/show change time,most recent last
 alias lu='ls -ltur'       # Sort by/show access time,most recent last
-alias lz='ls -lZ'# Show selinux context
+alias lz='ls -lZ'         # Show selinux context
 
 # The ubiquitous 'll': directories first, with alphanumeric sorting:
 alias ll="ls -lv --group-directories-first"
