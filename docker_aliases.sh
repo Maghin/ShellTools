acb71176eee35a34d91040f1b7e3da5548111e90
Maghin <realmaghin@hotmail.com>
2016-12-11 12:47:25 +0100

    Update docker_aliases.sh

--- docker_aliases.sh
+++ docker_aliases.sh
@@ -1,4 +1,6 @@
-alias docker-rm-all = 'docker rm `docker ps -qa`'
-alias docker-rmi-soft = 'docker rmi `docker images -qa -f dangling=true`'
-alias docker-rmi-all = 'docker rmi `docker images -qa`'
-alias docker-volume-rm-all = 'docker volume rm `docker volume ls -q`'
+alias docker_rm_all='docker rm `docker ps -qa`'
+alias docker_rmi_soft='docker rmi `docker images -qa -f dangling=true`'
+alias docker_rmi_all='docker rmi `docker images -qa`'
+alias docker_volume_rm_all='docker volume rm `docker volume ls -q`'
+
+alias docker_clean_all='docker_rm_all ; docker_rmi_all ; docker_volume_rm_all'
